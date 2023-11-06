921e4d7f524a8afceab3a77d94100a73f4f9b4ea
Thamara Perera <Thamaraperera2001@gmail.com>
2023-11-06 12:05:06 +0530

    Added header icon and resource links

--- src/Components/ProjectResources.jsx
+++ src/Components/ProjectResources.jsx
@@ -5,34 +5,37 @@ const ProjectResources = () => {
         <div id="projectResources" className="project-resources-container">
             <h1 className="primary-subheading">Project Resources</h1>
             <div className="project-resources-list">
-                <div className="resource-card">
+                {/* <div className="resource-card">
                     <h3>Project Charter</h3>
                     <a href="#">View</a>
-                </div>
+                </div> */}
                 <div className="resource-card">
                     <h3>Proposal Document</h3>
-                    <a href="#">View</a>
+                    <a href="https://mysliit.sharepoint.com/:w:/s/CDAPSubmissionCloud/EbAyYVM654RDhegxXOuA3AIBXOcvq5aNv1HqC9OL-0EhYA?e=1BKz2X">IT20246914 - Member 1</a>
+                    <a href="https://mysliit.sharepoint.com/:b:/s/CDAPSubmissionCloud/Ea-q0ni3qJJOsQh8a3yu2KQB4oThi5Sv7DaUhw3jFphovw?e=dMSxs9">IT20217440 - Member 2</a>
+                    <a href="https://mysliit.sharepoint.com/:b:/s/CDAPSubmissionCloud/Eb8uUSllQX9LuldrGAE92t4BccpVHRCGnJXRCvNtse4VAA?e=W0o8ir">IT20217136 - Member 3</a>
+                    <a href="https://mysliit.sharepoint.com/:b:/s/CDAPSubmissionCloud/EZUN3N4sJS9LkLnpmE-omiUBNdN2VO4QvSIt-mZ5z_dAPA?e=z2xUjU">IT20222086 - Member 4</a>
                 </div>
                 <div className="resource-card">
                     <h3>Status Documents</h3>
-                    <a href="#">Member 1</a>
-                    <a href="https://mysliit.sharepoint.com/:w:/s/CDAPSubmissionCloud/EbAyYVM654RDhegxXOuA3AIBXOcvq5aNv1HqC9OL-0EhYA?e=1BKz2X">IT20246914</a>
-                    <a href="#">Member 3</a>
-                    <a href="#">Member 4</a>
+                    <a href="https://mysliit.sharepoint.com/:w:/s/CDAPSubmissionCloud/EbAyYVM654RDhegxXOuA3AIBXOcvq5aNv1HqC9OL-0EhYA?e=1BKz2X">IT20246914 - Member 1</a>
+                    <a href="https://mysliit.sharepoint.com/:w:/s/CDAPSubmissionCloud/ERT5NAH3VPBEoboNtWHqKS4BmmmgfRoTcpE4hvB3gpDl8w?e=qw72Ri">IT20217440 - Member 2</a>
+                    <a href="https://mysliit.sharepoint.com/:w:/s/CDAPSubmissionCloud/EfdZok5apEJIj7Fg3AZiKKMBqLxilLCxwvGRqXpyzYZEew?e=75f10S">IT20217136 - Member 3</a>
+                    <a href="https://mysliit.sharepoint.com/:w:/s/CDAPSubmissionCloud/EWFLCKD2IGVDm8nQR3YPencBNRUNJ2bja8AW40ipYGp6aA?e=GUezpn">IT20222086 - Member 4</a>
                 </div>
                 <div className="resource-card">
                     <h3>Final Document</h3>
-                    <a href="#">Member 1</a>
-                    <a href="https://mysliit.sharepoint.com/:b:/s/CDAPSubmissionCloud/Ef3w5sfcaARGtM3UUEPEWK0BSlE7wVHHVTyeGxt_Mx3O7w?e=tvS4gn">IT20246914</a>
-                    <a href="#">Member 3</a>
-                    <a href="#">Member 4</a>
+                    <a href="https://mysliit.sharepoint.com/:b:/s/CDAPSubmissionCloud/Ef3w5sfcaARGtM3UUEPEWK0BSlE7wVHHVTyeGxt_Mx3O7w?e=tvS4gn">IT20246914 - Member 1</a>
+                    <a href="https://mysliit.sharepoint.com/:b:/s/CDAPSubmissionCloud/EZ8FSW3BesRKkgfDm53oqe0BD_BeE-9ONMAovjO_Vyxw3g?e=7LFRfx">IT20217440 - Member 2</a>
+                    <a href="https://mysliit.sharepoint.com/:b:/s/CDAPSubmissionCloud/ESY-QhKDIMpFnnxS7r4h2nwByfOnYUm_A63aqPw4_kxW8A?e=1W9Xyo">IT20217136 - Member 3</a>
+                    <a href="https://mysliit.sharepoint.com/:b:/s/CDAPSubmissionCloud/EVHcs2q3rpJLj4SBRgs5eRsBmU4Z_1o45h2Kp18QScre7Q?e=EDLbF9">IT20222086 - Member 4</a>
                 </div>
                 <div className="resource-card">
                     <h3>Presentation Slides</h3>
-                    <a href="#">Proposal Presentation</a>
-                    <a href="#">Progress Presentation 1 </a>
-                    <a href="#">Progess Presentation 2 </a>
-                    <a href="#">Final Presentation</a>
+                    {/* <a href="#">Proposal Presentation</a> */}
+                    <a href="https://mysliit.sharepoint.com/:p:/s/CDAPSubmissionCloud/ERHLLWyc2RRNiyPBNzhqxK0BiFsVfGFZbN9Amnen6c6BxA?e=8qIIK5">Progress Presentation 1 </a>
+                    <a href="https://mysliit.sharepoint.com/:p:/s/CDAPSubmissionCloud/EWAetYUW3IJBqjGEFPa-SJ4B3lH11F2xQi6aTMmdnRNXmg?e=HCOVIY">Progess Presentation 2 </a>
+                    <a href="https://mysliit.sharepoint.com/:p:/s/CDAPSubmissionCloud/EUSXvqY9lAxBsRDAKYxhemwBjQjAPxZslYRXzLiNKTssBA?e=6t8ncp">Final Presentation</a>
                 </div>
             </div>
         </div>
