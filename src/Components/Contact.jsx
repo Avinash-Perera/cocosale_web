54f9f1b72a7afa03f1551e01fbfecf3407be2c27
S.A.A Perera <it20246914@my.sliit.lk>
2023-11-06 11:29:02 +0530

    minor commit

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Contact = () => {
     return (
         <div id="contact" className="contact-page-wrapper">
-            <h1 className="primary-heading">Have Question In Mind?</h1>
+            <h1 className="primary-heading">Have Any Questions In Mind?</h1>
             <h1 className="primary-heading">Let Us Help You</h1>
             <div className="contact-form-container">
                 <input type="text" placeholder="Ask Your Question" />
